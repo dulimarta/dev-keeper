8228219ce443cb98aeb22180e291ef75fea72f8c
Hans Dulimarta <hans.dulimarta@gmail.com>
2013-05-27 16:23:10 -0400

    Device List activity now shows the data using two fragments in landscape mode.

--- PhoneCheckout/src/edu/gvsu/cis/dulimarh/checkout/PhoneCheckinActivity.java
+++ PhoneCheckout/src/edu/gvsu/cis/dulimarh/checkout/PhoneCheckinActivity.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -31,15 +32,10 @@ import com.parse.ParseQuery;
 import com.parse.SendCallback;
 
 public class PhoneCheckinActivity extends Activity {
+
     private final String TAG = getClass().getName();
     
-    private final static int DIALOG_CONFIRM_CHECKIN = 1;
-    private final static int DIALOG_WRONG_DEVICE = 2;
-    private String user_id, dev_id, parseObjId;
-    private TextView uid, devid, date;
-    private ImageView sig;
-    private ParseFile parseSignatureFile;
-    private Button checkin, ping;
+//    private String user_id, dev_id, parseObjId;
     
     /* (non-Javadoc)
      * @see android.app.Activity#onCreate(android.os.Bundle)
@@ -47,127 +43,17 @@ public class PhoneCheckinActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ParseQuery devQuery = new ParseQuery("DevOut");
         Intent data = getIntent();
-        user_id = data.getStringExtra("user_id");
-        dev_id = data.getStringExtra("dev_id");
-        setContentView(R.layout.checkin);
-        uid = (TextView) findViewById(R.id.user_id);
-        devid = (TextView) findViewById(R.id.dev_id);
-        date = (TextView) findViewById(R.id.out_date);
-        sig = (ImageView) findViewById(R.id.sig_imgview);
-        sig.setVisibility(View.INVISIBLE);
-        checkin = (Button) findViewById(R.id.checkin);
-        ping = (Button) findViewById(R.id.ping);
-        checkin.setEnabled(false);
-        devQuery.whereEqualTo("user_id", user_id);
+        int pos = data.getIntExtra("index", -1);
+        String user_id = data.getStringExtra("user_id");
+        String dev_id = data.getStringExtra("dev_id");
+        setContentView(R.layout.activity_checkin);
+        DeviceDetailsFragment ddf = DeviceDetailsFragment.newInstance(pos, user_id, dev_id);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.devdetails, ddf);
+        ft.commit();
         //        showDialog(DIALOG_PROGRESS);
         /* TODO: Add "Send email button"? */
-        checkin.setOnClickListener(new OnClickListener() {
-            
-            @Override
-            public void onClick(View v) {
-                Intent scan = new Intent("com.google.zxing.client.android.SCAN");
-                scan.putExtra("SCAN_MODE", "QR_CODE_MODE");
-                startActivityForResult(scan, 0);
-            }
-        });
-        ping.setOnClickListener(new PingHandler());
-        devQuery.findInBackground(new FindCallback() {
-            
-            @Override
-            public void done(List<ParseObject> arg0, ParseException arg1) {
-                if (arg1 == null) {
-                    if (arg0.size() == 1)
-                        try {
-                            ParseObject obj = arg0.get(0);
-                            parseObjId = obj.getObjectId();
-                            parseSignatureFile = (ParseFile) obj.get("signature");
-                            uid.setText(obj.getString("user_id"));
-                            devid.setText(obj.getString("dev_id"));
-                            date.setText(obj.getCreatedAt().toLocaleString());
-                            ByteArrayInputStream bis = new ByteArrayInputStream(parseSignatureFile.getData());
-                            Drawable d = Drawable.createFromStream(bis, "");
-                            sig.setImageDrawable(d);
-                            sig.setVisibility(View.VISIBLE);
-                            checkin.setEnabled(true);
-                            
-                        } catch (ParseException e) {
-                            Toast.makeText(
-                                    PhoneCheckinActivity.this,
-                                    "Failed to download signature of "
-                                            + user_id, Toast.LENGTH_LONG)
-                                    .show();
-                        }
-                    else {
-                        Toast.makeText(
-                                PhoneCheckinActivity.this,
-                                "Could not find a unique record", Toast.LENGTH_LONG)
-                                .show();
-                    }
-                }
-            }
-        });
-        
-    }
-    
-    /* Use an inner class in place of direct instance variable declaration
-     * to avoid "Parse.Initialize" error when this class is loaded by JVM
-     */
-    private class PingHandler implements OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-            try {
-                JSONObject pingMsg = new JSONObject(
-                        "{\"action\": \"edu.gvsu.cis.checkout.UPDATE\"," +
-                        "\"message\" : \"Ping\"}"
-                        );
-                ParsePush notify = new ParsePush();
-                notify.setChannel("Hans");
-                notify.setData(pingMsg);
-                Log.d(TAG, "Sending " + pingMsg.toString(3));
-                notify.sendInBackground(new SendCallback() {
-                    
-                    @Override
-                    public void done(ParseException arg0) {
-                        if (arg0 == null)
-                            Toast.makeText(PhoneCheckinActivity.this, "Ping delivered", 
-                                Toast.LENGTH_LONG).show();
-                        else
-                            Toast.makeText(PhoneCheckinActivity.this, 
-                                    "ParsePush error: " + arg0.getMessage(), 
-                                    Toast.LENGTH_LONG).show();
-                    }
-                });
-            } catch (JSONException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-        
-    }
-    /* (non-Javadoc)
-     * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-                final String contents = data.getStringExtra("SCAN_RESULT");
-                Bundle dialogData = new Bundle();
-                dialogData.putString("dev_id", dev_id);
-                if (contents.equals(dev_id))
-                    showDialog(DIALOG_CONFIRM_CHECKIN, dialogData);
-                else {
-                    dialogData.putString("scan_result", contents);
-                    showDialog(DIALOG_WRONG_DEVICE, dialogData);
-                }
-            }
-            else if (resultCode == RESULT_CANCELED) {
-                Log.d(TAG, "Scanning cancelled");
-            }
-        }
     }
 
     /* (non-Javadoc)
@@ -176,33 +62,13 @@ public class PhoneCheckinActivity extends Activity {
     @Override
     protected Dialog onCreateDialog(int id, Bundle args) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        switch (id) {
-        case DIALOG_CONFIRM_CHECKIN:
-            builder.setTitle("Confirmation Required");
-            builder.setMessage("Check in device " + args.getString("dev_id") + "?");
-            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    ParseQuery qr = new ParseQuery("DevOut");
-                    qr.getInBackground(parseObjId, new GetCallback() {
-
-                        @Override
-                        public void done(ParseObject arg0, ParseException arg1) {
-                            arg0.deleteInBackground();
-                            finish();
-                        }
-                    });
-                }
-            });
-            builder.setNegativeButton("No", null);
-            break;
-        case DIALOG_WRONG_DEVICE:
+//        switch (id) {
+//        case DIALOG_WRONG_DEVICE:
             builder.setTitle("Error: Unmatched IDs");
             builder.setMessage("Scanned id: " + args.getString("scan_result") + 
                     " device id: " + args.getString("dev_id"));
             builder.setPositiveButton("OK", null);
-        }
+//        }
         return builder.create();
     }
 
@@ -212,14 +78,14 @@ public class PhoneCheckinActivity extends Activity {
     @Override
     protected void onPrepareDialog(int id, Dialog dialog, Bundle args) {
         AlertDialog d = (AlertDialog) dialog;
-        switch (id) {
-        case DIALOG_CONFIRM_CHECKIN:
+//        switch (id) {
+//        case DIALOG_CONFIRM_CHECKIN:
             d.setMessage("Check in device " + args.getString("dev_id") + "?");
-            break;
-        case DIALOG_WRONG_DEVICE:
+//            break;
+//        case DIALOG_WRONG_DEVICE:
             d.setMessage("Scanned id: " + args.getString("scan_result") + 
                     " device id: " + args.getString("dev_id"));
-        }
+//        }
     }
 
     
--- PhoneCheckout/src/edu/gvsu/cis/dulimarh/checkout/PhoneListActivity.java
+++ PhoneCheckout/src/edu/gvsu/cis/dulimarh/checkout/PhoneListActivity.java
@@ -1,31 +1,16 @@
 package edu.gvsu.cis.dulimarh.checkout;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.ListActivity;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -37,13 +22,11 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 
-public class PhoneListActivity extends ListActivity {
+public class PhoneListActivity extends Activity {
     private final String TAG = getClass().getName();
     private static final int DIALOG_ALREADY_CHECKEDOUT = 1;
 //    private static final int DIALOG_PROGRESS = 2;
     
-    private SimpleAdapter adapter;
-    private ArrayList<Map<String,String>> checkouts;
     private ListView theList;
     /* (non-Javadoc)
      * @see android.app.Activity#onCreate(android.os.Bundle)
@@ -53,53 +36,11 @@ public class PhoneListActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         Parse.initialize(this, "AGs2nPlOxM7rA1BnUAbeVySTSRud6EhL7JF8sd4f",
                 "z5CgnppcixOqpAzHOdnTfT6ktKKzk6aicH8p1Rvb");
-        setContentView(R.layout.phonelist);
-        checkouts = new ArrayList<Map<String,String>>();
-        adapter = new SimpleAdapter(this, checkouts, android.R.layout.simple_list_item_2, 
-                new String[] {"dev_id", "user_id"},
-                new int[] {android.R.id.text1, android.R.id.text2});
-        theList = getListView();
-        setListAdapter(adapter);
-        theList.setOnItemLongClickListener(selectionListener);
+        setContentView(R.layout.activity_devlist);
+//        theList = getListView();
+//        theList.setOnItemLongClickListener(selectionListener);
     }
 
-    private OnItemLongClickListener selectionListener = new OnItemLongClickListener() {
-
-        @Override
-        public boolean onItemLongClick(AdapterView<?> arg0, View itemView,
-                int pos, long id) {
-            Map<String,String> entry = checkouts.get(pos);
-            return false;
-        }
-    };
-    
-    /* (non-Javadoc)
-     * @see android.app.Activity#onResume()
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        new CheckTask().execute();
-    }
-    
-    /* (non-Javadoc)
-     * @see android.app.ListActivity#onListItemClick(android.widget.ListView, android.view.View, int, long)
-     */
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        if (position >= 0 && position < checkouts.size())
-        {
-            Map<String,String> selected = checkouts.get(position);
-            String uid = selected.get("user_id");
-            String devid = selected.get("dev_id");
-            Intent i = new Intent(this, PhoneCheckinActivity.class);
-            i.putExtra("user_id", uid);
-            i.putExtra("dev_id", devid);
-            startActivity(i);
-        }
-        super.onListItemClick(l, v, position, id);
-    }
-    
     /*
      * (non-Javadoc)
      * 
@@ -140,45 +81,6 @@ public class PhoneListActivity extends ListActivity {
                 }
             }
         });
-    }
-
-    private class CheckTask extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                ParseQuery checkOutQuery = new ParseQuery("DevOut");
-                checkouts.clear();
-                for (ParseObject obj : checkOutQuery.find())
-                {
-                    Map<String,String> dev_u = new HashMap<String, String>();
-                    dev_u.put("dev_id", obj.getString("dev_id"));
-                    dev_u.put("user_id", obj.getString("user_id"));
-                    checkouts.add(dev_u);
-                }
-                Collections.sort(checkouts, new Comparator<Map<String,String>>() {
-
-                    @Override
-                    public int compare(Map<String, String> one,
-                            Map<String, String> two) {
-                        return one.get("user_id").compareTo(two.get("user_id"));
-                    }
-                });
-            } catch (ParseException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        /* (non-Javadoc)
-         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-         */
-        @Override
-        protected void onPostExecute(Void result) {
-            adapter.notifyDataSetChanged();
-        }
-        
     }
 
     /* (non-Javadoc)
