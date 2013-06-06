a10b3441957197cc885660d31252df57e4e1c1f2
Hans Dulimarta [Macbook] <hans.dulimarta@gmail.com>
2013-06-05 22:12:56 -0400

    Complete the feature for uploading userid/username from Google Spreadsheet to Parse server.
    Add a "Cancel" button to stop the asynctask for downloading the list of spreadsheets.

--- PhoneCheckout/src/edu/gvsu/cis/dulimarh/checkout/GoogleDriveListFragment.java
+++ PhoneCheckout/src/edu/gvsu/cis/dulimarh/checkout/GoogleDriveListFragment.java
@@ -17,7 +17,10 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.ListFragment;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -25,10 +28,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.GoogleAuthException;
@@ -47,23 +55,22 @@ import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
 import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
 import com.google.gdata.data.spreadsheet.WorksheetEntry;
 import com.google.gdata.util.ServiceException;
-//import java.util.List;
-//import com.google.gdata.client.spreadsheet.SpreadsheetService;
-//import com.google.gdata.data.ParseSource;
-//import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
-//import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
-//import com.google.gdata.data.spreadsheet.WorksheetEntry;
-//import com.google.gdata.util.ServiceException;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 public class GoogleDriveListFragment extends ListFragment {
-   final static int REQUEST_SELECT_ACCT = 1;
-   final static int REQUEST_AUTH = 2;
+   private final static int REQUEST_SELECT_ACCT = 1;
+   private final static int REQUEST_AUTH = 2;
+   private final static int COLUMN_ZERO_USERID = 0;
+   private final static int COLUMN_ZERO_USERNAME = 1;
    private static String SPREADSHEET_SCOPE = 
          "https://spreadsheets.google.com/feeds";
    private static String SPREADSHEET_FEED_URL = 
          SPREADSHEET_SCOPE + "/spreadsheets/";
    private String TAG = getClass().getName();
    private GoogleAccountCredential credential;
+   private static Context context;
    private Drive service;
    private SpreadsheetService ssService;
    private String acctName;
@@ -72,8 +79,11 @@ public class GoogleDriveListFragment extends ListFragment {
    private SimpleAdapter adapter;
    private ListView theList;
    private ProgressBar loadProgress;
+   private ImageButton cancelLoading;
+   private TextView status;
    private boolean listFileTaskRunning;
    private String[] users;
+   private ListFileTask listFileTask;
    
    /* (non-Javadoc)
     * @see android.app.Fragment#onCreate(android.os.Bundle)
@@ -82,9 +92,11 @@ public class GoogleDriveListFragment extends ListFragment {
    public void onCreate(Bundle savedInstanceState) {
       // TODO Auto-generated method stub
       super.onCreate(savedInstanceState);
+      context = getActivity();
       allFiles = new ArrayList<Map<String,String>>();
 //      allWorksheets = new ArrayList<WorksheetEntry>();
       listFileTaskRunning = false;
+      listFileTask = null;
       adapter = new SimpleAdapter(getActivity(), allFiles, 
             android.R.layout.simple_list_item_2, 
             new String[] {"name", "columns"}, 
@@ -100,7 +112,16 @@ public class GoogleDriveListFragment extends ListFragment {
          Bundle savedInstanceState) {
       View view = inflater.inflate(R.layout.fragment_gdrivelist, container, false);
       loadProgress = (ProgressBar) view.findViewById(R.id.gdrive_progress);
-      
+      cancelLoading = (ImageButton) view.findViewById(R.id.cancel_loading);
+      status = (TextView) view.findViewById(R.id.text_info);
+      cancelLoading.setOnClickListener(new OnClickListener() {
+         
+         @Override
+         public void onClick(View v) {
+            if (listFileTask != null && listFileTaskRunning)
+               listFileTask.cancel(true);
+         }
+      });
       /* Allow multiple scopes: Drive and Spreadsheet APIs */
       credential = GoogleAccountCredential.usingOAuth2(getActivity(), 
             DriveScopes.DRIVE, SPREADSHEET_SCOPE);
@@ -124,7 +145,8 @@ public class GoogleDriveListFragment extends ListFragment {
          service = getDriveService (credential);
          ssService = new SpreadsheetService("HansDevCheckout");
          ssService.setProtocolVersion(SpreadsheetService.Versions.V3);
-         new ListFileTask().execute();
+         listFileTask = new ListFileTask();
+         listFileTask.execute();
       }
       else
          startActivityForResult(credential.newChooseAccountIntent(),
@@ -163,7 +185,8 @@ public class GoogleDriveListFragment extends ListFragment {
                   prefEditor = getActivity().getApplicationContext().getSharedPreferences("dev_checkout", Context.MODE_PRIVATE).edit();
                   prefEditor.putString("acctName", acctName);
                   prefEditor.commit();
-                  new ListFileTask().execute();
+                  listFileTask = new ListFileTask();
+                  listFileTask.execute();
                } catch (Exception e) {
                   Log.e(TAG, "Generated exception: " + e.getMessage());
                   e.printStackTrace();
@@ -173,7 +196,8 @@ public class GoogleDriveListFragment extends ListFragment {
          break;
       case REQUEST_AUTH:
          if (requestCode == Activity.RESULT_OK) {
-            new ListFileTask().execute();
+            listFileTask = new ListFileTask();
+            listFileTask.execute();
          }
          break;
       }
@@ -185,8 +209,6 @@ public class GoogleDriveListFragment extends ListFragment {
    @Override
    public void onListItemClick(ListView l, View v, int position, long id) {
       Map<String,String> selection = allFiles.get(position);
-      Toast.makeText(getActivity(), "Selecting " + selection.get("name"), 
-            Toast.LENGTH_LONG).show();
       new FileDownloadTask().execute(selection.get("name"),
             selection.get("url"), selection.get("rowCount")); 
    }
@@ -208,7 +230,7 @@ public class GoogleDriveListFragment extends ListFragment {
             }
          };
 
-   private class ListFileTask extends AsyncTask<Void, Integer, Void> {
+   private class ListFileTask extends AsyncTask<Void, Integer, Integer> {
 
       /* (non-Javadoc)
        * @see android.os.AsyncTask#onPreExecute()
@@ -218,11 +240,15 @@ public class GoogleDriveListFragment extends ListFragment {
          listFileTaskRunning = true;
          theList = getListView();
          theList.setVisibility(View.GONE);
+         status.setText(R.string.label_gdrive_loading);
+         loadProgress.setIndeterminate(true);
+         loadProgress.setInterpolator(new BounceInterpolator());
          loadProgress.setVisibility(View.VISIBLE);
+         cancelLoading.setVisibility(View.VISIBLE);
       }
 
       @Override
-      protected Void doInBackground(Void... params) {
+      protected Integer doInBackground(Void... params) {
          try {
             
             /* build the GoogleCredential from GoogleAccountCredential */
@@ -239,7 +265,9 @@ public class GoogleDriveListFragment extends ListFragment {
             int k = 0;
             allFiles.clear();
 //            allWorksheets.clear();
+            loadProgress.setIndeterminate(false);
             for (SpreadsheetEntry e : entries) {
+               if (isCancelled()) break;
                publishProgress(k, totalEntries);
                WorksheetEntry ws = e.getDefaultWorksheet();
 //               allWorksheets.add(ws);
@@ -269,6 +297,7 @@ public class GoogleDriveListFragment extends ListFragment {
                k++;
             }
             Collections.sort(allFiles, fileNameComparator);
+            return allFiles.size();
          } 
          catch (UserRecoverableAuthIOException ioauth)
          {
@@ -287,10 +316,22 @@ public class GoogleDriveListFragment extends ListFragment {
             // TODO Auto-generated catch block
             e1.printStackTrace();
          }
-        return null;
+        return 0;
       }
 
-      
+      /* (non-Javadoc)
+       * @see android.os.AsyncTask#onCancelled(java.lang.Object)
+       */
+      @Override
+      protected void onCancelled(Integer result) {
+         loadProgress.setVisibility(View.GONE);
+         cancelLoading.setVisibility(View.GONE);
+         status.setText(R.string.label_gdrive_select);
+         Toast.makeText(getActivity(), "Operation cancelled", 
+               Toast.LENGTH_LONG).show();
+         listFileTaskRunning = false;
+      }
+
       /* (non-Javadoc)
        * @see android.os.AsyncTask#onProgressUpdate(Progress[])
        */
@@ -304,16 +345,25 @@ public class GoogleDriveListFragment extends ListFragment {
        * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
        */
       @Override
-      protected void onPostExecute(Void result) {
+      protected void onPostExecute(Integer result) {
          listFileTaskRunning = false;
          theList.setVisibility(View.VISIBLE);
          loadProgress.setVisibility(View.GONE);
-         adapter.notifyDataSetChanged();
+         cancelLoading.setVisibility(View.GONE);
+         if (result > 0)
+         {
+            status.setText(R.string.label_gdrive_select);
+            adapter.notifyDataSetChanged();
+         }
+         else
+            Toast.makeText(getActivity(), "Error loading spreadsheets", 
+                  Toast.LENGTH_LONG).show();
       }
    }
 
    private class FileDownloadTask extends AsyncTask<String, Integer, Integer>
    {
+      private String fileName;
       /* (non-Javadoc)
        * @see android.os.AsyncTask#onPreExecute()
        */
@@ -324,25 +374,30 @@ public class GoogleDriveListFragment extends ListFragment {
 
       @Override
       protected Integer doInBackground(String... params) {
+         fileName = params[0];
          int rows = Integer.parseInt(params[2]);
          String[][] tempUsers = new String [rows][2];
-         users = new String[rows];
          try {
             URL cFeedURL = new URI(params[1] +
                   "?max-col=2&min-row=2&max-row=" + params[2]).toURL();
             CellFeed cFeed = ssService.getFeed(cFeedURL, CellFeed.class);
-            int k = 0;
+            int count = 0;
             for (CellEntry e: cFeed.getEntries())
             {
                Cell c = e.getCell();
-               Log.d(TAG, "R" + c.getRow() + "C" + c.getCol() + " => " +
-                     c.getValue());
                /* since we begin at row 2, we want to subtract row index by 2 */
-               tempUsers[c.getRow()-2][c.getCol()-1] = c.getValue();
-               publishProgress(k, rows);
-               k++;
+               if (c.getValue() == null)
+                  break;
+               tempUsers[c.getRow() - 2][c.getCol() - 1] = c.getValue();
+               publishProgress(count, rows);
+               
+               if (count < c.getRow())
+                  count = c.getRow();
             }
-            for (k = 0; k < tempUsers.length; k++)
+            count--;
+            users = null;
+            users = new String[count];
+            for (int k = 0; k < count; k++)
                users[k] = tempUsers[k][0] + "#::#" + tempUsers[k][1];
                
          } catch (Exception e) {
@@ -365,26 +420,107 @@ public class GoogleDriveListFragment extends ListFragment {
       protected void onPostExecute(Integer result) {
 //         theList.setVisibility(View.VISIBLE);
          loadProgress.setVisibility(View.GONE);
-         showDialog(result);
+         showDialog(fileName, result);
       }
    }
    
-   private void showDialog(int numRows)
+   private static class UploadToParseTask extends AsyncTask<Object, String, Integer>
+   {
+
+      private ProgressDialog pdiag;
+      
+      /* (non-Javadoc)
+       * @see android.os.AsyncTask#onPreExecute()
+       */
+      @Override
+      protected void onPreExecute() {
+         pdiag = new ProgressDialog(context);
+         pdiag.show();
+      }
+
+      @Override
+      protected Integer doInBackground(Object... params) {
+         String[] users = (String[]) params[0];
+         String[] toks;
+         pdiag.setMax(users.length);
+         int layout = (Integer) params[1];
+         int k = 0;
+         try {
+            ParseQuery query = new ParseQuery("TestUsers");
+            pdiag.setTitle("Deleting current users");
+            for (ParseObject obj : query.find())
+            {
+               publishProgress("Deleting " + obj.getString("user_id"));
+               obj.delete();
+            }
+            pdiag.setTitle("Uploading new users");
+            for (String data : users) {
+               toks = data.split("#::#");
+               ParseObject userObj = new ParseObject("TestUsers");
+               if (toks[0] != null && toks[1] != null ||
+                     ! ("null".equals(toks[0]) || "null".equals(toks[1]))) {
+                  if (layout == COLUMN_ZERO_USERID) {
+                     publishProgress(k + "/" + users.length + " " + toks[0]);
+                     userObj.put("user_id", toks[0]);
+                     userObj.put("user_name", toks[1]);
+                  } else {
+                     publishProgress(k + "/" + users.length + " " + toks[1]);
+                     userObj.put("user_id", toks[1]);
+                     userObj.put("user_name", toks[0]);
+                  }
+                  userObj.save();
+               }
+               k++;
+            }
+            return users.length;
+         } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+         }
+         return 0;
+      }
+      
+      /* (non-Javadoc)
+       * @see android.os.AsyncTask#onProgressUpdate(Progress[])
+       */
+      @Override
+      protected void onProgressUpdate(String... values) {
+         pdiag.setMessage(values[0]);
+      }
+
+      /* (non-Javadoc)
+       * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+       */
+      @Override
+      protected void onPostExecute(Integer result) {
+         pdiag.dismiss();
+         if (result == 0)
+            Toast.makeText(context, "Error uploading data to Parse server", 
+                  Toast.LENGTH_LONG).show();
+      }
+
+   }
+
+   private void showDialog(String file, int numRows)
+   
    {
       DialogFragment diafrag;
       if (numRows > 0)
-         diafrag = UploadConfirmDialog.newInstance(users);
+         diafrag = UploadConfirmDialog.newInstance(file, users);
       else
-         diafrag = EmptyListWarningDialog.newInstance();
+         diafrag = EmptyListWarningDialog.newInstance(file);
       diafrag.show(getFragmentManager(), "dialog");
    }
    
    public static class UploadConfirmDialog extends DialogFragment {
-
-      public static UploadConfirmDialog newInstance(String[] _userList)
+      private int columnLayout;
+      private String[] usrList;
+      
+      public static UploadConfirmDialog newInstance(String file, String[] _userList)
       {
          UploadConfirmDialog frag = new UploadConfirmDialog();
          Bundle args = new Bundle();
+         args.putString("filename", file);
          args.putStringArray("users", _userList);
          frag.setArguments(args);
          return frag;
@@ -396,31 +532,47 @@ public class GoogleDriveListFragment extends ListFragment {
       @Override
       public Dialog onCreateDialog(Bundle savedInstanceState) {
          AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-         String[] usrList;
          usrList = getArguments().getStringArray("users");
          String[] toks = usrList[0].split("#::#");
-         builder.setTitle("Select column layout");
-         builder.setItems(new String[] {
+         builder.setTitle("Select column layout of \"" + 
+               getArguments().getString("filename") + "\"\n" + 
+               "WARNING: existing users will be deleted");
+         builder.setSingleChoiceItems(new String[] {
                "userid:" + toks[0] + " username:" + toks[1],
                "userid:" + toks[1] + " username:" + toks[0],
-         }, null);
+         }, 0, new DialogInterface.OnClickListener() {
+            
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               columnLayout = which;
+            }
+         });
          builder.setNegativeButton("Cancel", null);
-         builder.setPositiveButton("Upload", null);
+         builder.setPositiveButton("Upload", new DialogInterface.OnClickListener() {
+            
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               new UploadToParseTask().execute(usrList, columnLayout);
+            }
+         });
          return builder.create();
       }
    }
-   
+      
    public static class EmptyListWarningDialog extends DialogFragment {
-      public static EmptyListWarningDialog newInstance()
+      public static EmptyListWarningDialog newInstance(String file)
       {
          EmptyListWarningDialog frag = new EmptyListWarningDialog();
+         Bundle args = new Bundle();
+         args.putString("filename",file);
          return frag;
       }
 
       @Override
       public Dialog onCreateDialog(Bundle savedInstanceState) {
          AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-         builder.setMessage("Cannot upload from an empty spreadsheet");
+         builder.setMessage("Cannot upload from an empty spreadsheet " +
+               getArguments().getString("filename"));
          builder.setPositiveButton("OK", null);
          return builder.create();
       }
